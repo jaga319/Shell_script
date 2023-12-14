a031b33793f25d4254827753fc27a76fd59e6bbf
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-14 22:30:17 +0530

    2nd script is updated by variable

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-echo "Ramesh:how are u"
-echo "Suresh: iam fine u"
-echo "Ramesh: fine thank you"
-echo "suresh: how is your work"
-echo "Ramesh: work is good "
+name1=Ramesh
+name2=suresh
+echo "$name1:how are u"
+echo "$name2: iam fine u"
+echo "$name1: fine thank you"
+echo "$name2: how is your work"
+echo "$name1: work is good "
