a2b5744ba8a146b89b0f85a40f70916759c240b1
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-15 23:08:08 +0530

    arg variable

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,3 +6,13 @@ person2=$2
 
 echo "$person1: Hi $person2"
 echo "$person2: Hi $person1"
+
+echo "Type 2"
+
+echo "Please enter your username :"
+read username
+echo "Please enter your password :"
+read -s password 
+
+echo "The username is $username"
+echo "The password is $password"
