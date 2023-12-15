9b764e732617ad78d9b65e045ed7d1f2d7c29169
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-16 00:13:31 +0530

    arg variable

---
+++ 06-installSQL.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ID=$(id -u)
+if [ $ID -e 0 ]
+then
+   yum install mysql -y
+else
+   echo "Login to root user and then try"
+   exit 1
+fi
+
+if [ &? -ne 0 ]
+then 
+   echo "Error:Installing mysql is failed"
+else
+   echo "Install mysql successfully"
+fi
+
