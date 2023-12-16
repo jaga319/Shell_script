05bec1e5c294dea63c4bdff0a6e1ca1068828d38
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-16 13:14:16 +0530

    practicing git

--- 08-logs.sh
+++ 08-logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-LOG_File=/tmp/
+Date=$(date +%F-%H-%M-%S)
+LOG_File="/tmp/$0-$Date.log"
 validate(){
     if [ $1 -ne 0 ]
 then 
@@ -17,11 +18,11 @@ else
    echo "Error:Login to root user"
    exit 1
 fi
-   yum install mysql -y
+   yum install mysql -y &>>LOG_File
 
 validate $? MYSQL
 
-   yum install git -y
+   yum install git -y &>>LOG_File
    
 validate $? GIT
 
