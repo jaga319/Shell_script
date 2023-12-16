1baec8490f84153f918d270d6b9ff8c2e39bc683
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-16 23:10:38 +0530

    Logs file created

--- 08-logs.sh
+++ 08-logs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 Date=$(date +%F-%H-%M-%S)
 LOG_File="/tmp/$0-$Date.log"
+R=\e[31m
+N=\e[0m
 validate(){
     if [ $1 -ne 0 ]
 then 
-   echo "Error:Installation $2 is failed"
+   echo "$R Error:$N Installation $2 is failed"
 else
    echo "Installation $2 successfully"
 fi
@@ -15,7 +17,7 @@ then
    echo "script name : $0 "
    echo "You are the root user"
 else
-   echo "Error:Login to root user"
+   echo "$R Error:$n Login to root user"
    exit 1
 fi
    yum install mysql -y &>> $LOG_File
