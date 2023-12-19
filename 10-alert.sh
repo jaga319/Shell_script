f0a4f87968be63263804fb1b1353724dab3afce9
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-19 23:09:25 +0530

    disk script

--- 10-alert.sh
+++ 10-alert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 disk_space=$(df -h)
 D=awk '$5>10 {print $5}'
-if [ D -gt 10 ]
+if [ $D -gt 10 ]
 then
    echo "$disk_space" | awk '$5>10 {print $1}' 
    echo "The above disk space is more than 90% . Please take neccesary action "
