64d8ac942a61b1939eb6db217068cac68f156051
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-19 22:56:42 +0530

    disk script

--- 10-alert.sh
+++ 10-alert.sh
@@ -2,4 +2,4 @@
 disk_space=$(df -h)
 
 echo "$disk_space" | awk '$5>10 {print $1}' 
-echo " The abpve disk space is more than 90% . Please take neccesary action "
+echo " The above disk space is more than 90% . Please take neccesary action "
