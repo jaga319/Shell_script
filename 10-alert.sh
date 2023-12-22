9b175e3324045dcdf6504957f0344db7b93d8684
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-22 14:15:49 +0530

    alert_diskspace

--- 10-alert.sh
+++ 10-alert.sh
@@ -16,12 +16,13 @@ disk_space=$(df -h)
 # Use a while loop to read lines of the df output
 echo "$disk_space" | tail -n +2 | while read -r line; do
     # Extract the filesystem and percentage values
-    filesystem=$(echo "$line" | awk '{print $1}')
-    percentage=$(echo "$line" | awk '{print $5}' | sed 's/[^0-9]//g')
+    echo "$line"
+   #  filesystem=$(echo "$line" | awk '{print $1}')
+   #  percentage=$(echo "$line" | awk '{print $5}' | sed 's/[^0-9]//g')
 
-    # Check if the percentage is greater than 1
-    if [ "$percentage" -gt 0 ]; then
-        echo "$filesystem - $percentage%"
-        echo "The above disk space is more than 1%. Please take necessary action."
-    fi
+   #  # Check if the percentage is greater than 1
+   #  if [ "$percentage" -gt 0 ]; then
+   #      echo "$filesystem - $percentage%"
+   #      echo "The above disk space is more than 1%. Please take necessary action."
+   #  fi
 done
