41a519422e7f32ae266b3035514f86593a01a660
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-22 13:51:06 +0530

    alert_diskspace

--- 10-alert.sh
+++ 10-alert.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 disk_space=$(df -h)
 D=$(echo "$disk_space" | awk '$5>1 {print $5}' | sed 's/[^0-9]//g')
-echo "$D"
-# if [ $D -gt 10 ]
-# then
-#    echo "$disk_space" | awk '$5>10 {print $1 " - " $5}' 
-#    echo "$D"
-#    echo "The above disk space is more than 90% . Please take neccesary action "
-# fi
+for S in $D
+do
+if [ $S -gt 1 ]
+then
+   echo "$disk_space" | awk '$5>10 {print $1 " - " $5}' 
+   echo "$D"
+   echo "The above disk space is more than 90% . Please take neccesary action "
+fi
+done
