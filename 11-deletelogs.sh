e443d6ef78f4103586401d65fb292dcc84baadf3
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-22 23:31:30 +0530

    delete-logs

---
+++ 11-deletelogs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source_dir=/tmp/shellscript-logs
+R="\e[31m"
+G="\e[32m"
+$N="\e[0m"
+if [ ! -d $source_dir ]
+then 
+   echo -e "$R $source_dir source directory is not available $N"
+   exit 1
+fi
+cd $source_dir
+
+FILES_TO_DELETE=$(find . -type f -mtime +14 -name "*.log")
+while IFS=  read -r line
+do
+  echo "deleting file : $line"
+done <<< $FILES_TO_DELETE
+
+
