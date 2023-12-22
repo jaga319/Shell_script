97bd1954343a1bd777122747f89e2fdba12aa260
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-22 14:06:12 +0530

    alert_diskspace

--- 10-alert.sh
+++ 10-alert.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 disk_space=$(df -h)
-D=$(echo "$disk_space" | awk '$5>1 {print $5}' | sed 's/[^0-9]//g')
-for S in $D
-do
-if [ $S -gt 1 ]
-then
-   echo "$disk_space" | awk '$5>1 {print $1 " -         " $5}' 
-   echo "The above disk spaces is more than 1% . Please take neccesary action "
-fi
-done
+echo "$disk_space" | tail -n +2 
+# D=$(echo "$disk_space" | awk '$5>1 {print $5}' | sed 's/[^0-9]//g')
+# for S in $D
+# do
+# if [ $S -gt 1 ]
+# then
+#    echo "$disk_space" | awk '$5>1 {print $1 " -" $5}' 
+#    echo "The above disk spaces is more than 1% . Please take neccesary action "
+# fi
+# done
