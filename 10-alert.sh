618cd9cc307b0f837323abc1fe1e264a1920cc2a
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-22 13:59:34 +0530

    alert_diskspace

--- 10-alert.sh
+++ 10-alert.sh
@@ -5,7 +5,7 @@ for S in $D
 do
 if [ $S -gt 1 ]
 then
-   echo "$disk_space     " | awk '$5>1 {print $1 " - " $5}' 
+   echo "$disk_space" | awk '$5>1 {print $1 " -         " $5}' 
    echo "The above disk spaces is more than 1% . Please take neccesary action "
 fi
 done
