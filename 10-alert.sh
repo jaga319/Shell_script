47176a82c4a85d8a2b66b1a27206a26d8fb2a921
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-22 13:56:21 +0530

    alert_diskspace

--- 10-alert.sh
+++ 10-alert.sh
@@ -6,6 +6,6 @@ do
 if [ $S -gt 1 ]
 then
    echo "$disk_space     " | awk '$5>1 {print $1 " - " $5}' 
-   echo "The above disk space is more than 90% . Please take neccesary action "
+   echo "The above disk spaces is more than 1% . Please take neccesary action "
 fi
 done
