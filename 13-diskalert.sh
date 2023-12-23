9b6a52a19875c13315fbf84eded94371b1e1bd7a
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-23 13:56:03 +0530

    practicing git

--- 13-diskalert.sh
+++ 13-diskalert.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 disk_usage=$(df -hT | grep -vE 'tmp|File')
 # message="The disk space is more than $thresold% . Please take neccesary action "
-FLAG=FALSE
+FLAG="FALSE"
 while IFS= read  line 
 do
     useage=$(echo $line| awk '{print $6F}'|cut -d % -f1)
     filesystem=$(echo "$line" | awk '{print $1F}')
     if [ $useage -ge 1 ]
     then 
-       FLAG=TRUE
+       FLAG="TRUE"
        echo "HIGH DISK USAGE $filesystem : $useage"
     fi
 done <<< $disk_usage
