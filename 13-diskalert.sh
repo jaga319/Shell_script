63e6cd3bbe0e47ac39d5ee6f9035c051f196704d
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-23 17:24:48 +0530

    practicing git

--- 13-diskalert.sh
+++ 13-diskalert.sh
@@ -3,6 +3,7 @@ disk_usage=$(df -hT | grep -vE 'tmp|File')
 # message="The disk space is more than $thresold% . Please take neccesary action "
 FLAG1="FALSE"
 FLAG2="TRUE"
+message=""
 while IFS= read  line 
 do
     useage=$(echo $line| awk '{print $6F}'|cut -d % -f1)
@@ -10,14 +11,14 @@ do
     if [ $useage -ge 1 ]
     then 
        FLAG1="TRUE"
-       echo "$filesystem : $useage"
+       message+=echo "$filesystem : $useage \n"
     fi
 done <<< $disk_usage
 
 if [ "$FLAG1" == "$FLAG2" ]
 then
-   message="please check above disk spaces are more than 1%"
-   echo "$message" | mail -s "High Disk Space Usage" mummadijagadeesh219@gmail.com
+   message2="please check above disk spaces are more than 1%"
+   echo "$message \n message2" | mail -s "High Disk Space Usage" mummadijagadeesh219@gmail.com
 else 
    echo "All disk spaces are fine" | mail -s "High Disk Space Usage" mummadijagadeesh219@gmail.com
 fi
