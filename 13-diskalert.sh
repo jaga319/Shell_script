e57a71a067577177b47a0440924b2ce60bbad9ef
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-23 17:34:14 +0530

    practicing git

--- 13-diskalert.sh
+++ 13-diskalert.sh
@@ -18,7 +18,7 @@ done <<< $disk_usage
 if [ "$FLAG1" == "$FLAG2" ]
 then
    message2="please check above disk spaces are more than 1%"
-   echo "$message\n$message2" | mail -s "High Disk Space Usage" mummadijagadeesh219@gmail.com
+   echo -e "$message\n$message2" | mail -s "High Disk Space Usage" mummadijagadeesh219@gmail.com
 else 
    echo "All disk spaces are fine" | mail -s "High Disk Space Usage" mummadijagadeesh219@gmail.com
 fi
