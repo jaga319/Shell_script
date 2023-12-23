df0270195023f11947644e102a49b2d69eb95708
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-23 12:09:21 +0530

    practicing git

---
+++ 12-IFS.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+file="/etc/passwd"
+
+if [ ! -f $file ]
+then
+    echo "The file $file does not exist"
+fi
+ 
+while IFS=":" read -r username password user_id groud_id user_fullname home_dir shell_path
+do 
+   echo "username: $username"
+   echo "user_id:$user_id"
+   echo "user_fullname:$user_fullname"
+     
+done <<< $file
+
