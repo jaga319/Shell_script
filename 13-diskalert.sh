a698141e13ea6df5d5d2d01fa4394e7b0a2ee6cd
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-23 13:49:43 +0530

    practicing git

--- 13-diskalert.sh
+++ 13-diskalert.sh
@@ -5,7 +5,7 @@ while IFS= read  line
 do
     useage=$(echo $line| awk '{print $6F}'|cut -d % -f1)
     filesystem=$(echo "$line" | awk '{print $1F}')
-    if [ $useage -gt 1 ]
+    if [ $useage -ge 1 ]
     then 
        echo "HIGH DISK USAGE $filesystem : $useage"
     fi
