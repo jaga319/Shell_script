34591a23af8f94a642baef08ee4ceceb4f9238e5
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-24 16:47:38 +0530

    instance creating

--- 14-instance.sh
+++ 14-instance.sh
@@ -11,7 +11,9 @@ do
         INSTANCES_TYPE="t3.small"
     else
         INSTANCES_TYPE="t2.micro"
-    fi 
+    fi
+    if [ ! $i == "web" ] 
+    then
     IP_Address=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCES_TYPE --security-group-ids sg-0ef3f217f9042a8d7 --count 1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     aws route53 change-resource-record-sets \
     --hosted-zone-id Z0685417SSJQDRXEKNGA \
@@ -31,5 +33,26 @@ do
         }]
     }
     '
+    else
+      IP_Address=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCES_TYPE --security-group-ids sg-0ef3f217f9042a8d7 --count 1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text)
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id Z0685417SSJQDRXEKNGA \
+    --change-batch '
+    {
+        "Comment": "creating a record set"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.infome.website"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_Address'"
+            }]
+        }
+        }]
+    }
+    '
+    fi
 done
 
