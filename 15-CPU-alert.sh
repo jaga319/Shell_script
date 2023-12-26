8fa436ca6168c574406c04a144ae8928f38bed05
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-26 12:43:14 +0530

    cpu alert

--- 15-CPU-alert.sh
+++ 15-CPU-alert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 C=$(top -n 1 -b)
 host=$(hostname)
-CP=$(echo "$C" | awk '$1=="%Cpu(s):" {print int($2) }') 
+CP=$(echo "$C" | awk '$1=="%Cpu(s):" {print int($2) }')
 # if [ $CP -gt 90 ];then 
 #   echo "$host is High CPU $CP% PLEASE CHECK"
 # else
