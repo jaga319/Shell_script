e9f61724b47ccd69b01bd86ef0d8eec4a177c05a
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-26 13:51:24 +0530

    cpu alert

--- 15-CPU-alert.sh
+++ 15-CPU-alert.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+# C=$(top -n 1 -b)
+# host=$(hostname)
+# CP=$(echo "$C" | awk '$1=="%Cpu(s):" { print int($2) }')
+# if [ $CP -gt 2 ];then 
+#   echo "$host is High CPU $CP% PLEASE CHECK"
+# else
+#   echo "CPU % IS FINE " 
+# fi
+
+# echo "$CP"
+# # echo "$host"
+# # echo "$C"
+
+# # CP=$(echo "$C" | awk '{print $1 }') -- %Cpu(s):
+
 C=$(top -n 1 -b)
 host=$(hostname)
-CP=$(echo "$C" | awk '$1=="%Cpu(s):" { print int($2) }')
-if [ $CP -gt 2 ];then 
+CP=$(echo "$C" | awk '/^%Cpu\(s\):/ {gsub(/,/, ""); print int($2)}' 2>/dev/null || echo 0)
+
+echo "Host: $host"
+echo "Top Output:"
+echo "$C"
+echo "CPU Percentage: $CP"
+
+if [ "$CP" -gt 90 ]; then 
   echo "$host is High CPU $CP% PLEASE CHECK"
 else
-  echo "CPU % IS FINE " 
+  echo "CPU % IS FINE" 
 fi
-
-echo "$CP"
-# echo "$host"
-# echo "$C"
-
-# CP=$(echo "$C" | awk '{print $1 }') -- %Cpu(s):
