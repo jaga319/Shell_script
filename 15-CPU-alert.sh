65a5e3af5a47f9e777eecc70725363b40465c15e
jaga319 <mummadijagadeesh219@gmail.com>
2023-12-26 11:51:37 +0530

    cpu alert

--- 15-CPU-alert.sh
+++ 15-CPU-alert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 C=$(top -n 1 -b)
 # CP=$(echo "$C" | awk '$1=="%Cpu(s) :" {print $1 " - " $2 }') 
-CP=$(echo "$C" | awk {print $1 }) 
+CP=$(echo "$C" | awk '{print $1 }') 
 echo "$CP"
 
